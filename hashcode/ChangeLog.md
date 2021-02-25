5d9f568fe433e30be96218fb33c555c8540d6e90
ajuvercr <arthur.vercruysse@outlook.com>
2021-02-25 18:59:04 +0100

    setup hashcode

---
+++ hashcode/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for haskcode
+
+## Unreleased changes
---
+++ hashcode/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "BUILDING"
+stack build
+zip out/source.zip src/* app/* stack.yaml package.yaml
+
+for file in $(ls input | grep "\.txt\$")
+do
+    echo $file
+  ./haskcode-exe res/$file > out/$file
+done
