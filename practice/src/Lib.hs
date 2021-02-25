256286f098081c17a1ee3d9e7a3372fbc71de217
ajuvercr <arthur.vercruysse@outlook.com>
2021-02-25 13:28:44 +0100

    string are numbers

--- practice/src/Lib.hs
+++ practice/src/Lib.hs
@@ -4,6 +4,12 @@ module Lib
 
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+import Control.Monad.State.Lazy (State)
+import qualified Control.Monad.State.Lazy as State
+
 import System.IO
 import System.Environment
 
@@ -30,18 +36,38 @@ maxBy f (x:xs) = let b = maxBy f xs in
         then b
         else x
 
+fromMaybe :: a -> Maybe a -> a
+fromMaybe a Nothing = a
+fromMaybe _ (Just a) = a
 
 type Teams = (Int, Int, Int) -- #2 persons, #3 persons, #4 persons
-type Pizza = (Set String, Int)
+type Pizza = (Set Int, Int)
 type Output = (Int, [Int]) -- teamsize, pizzas
+
+getOrInsertDefault :: Ord k => k -> a -> Map k a -> (a, Map k a)
+getOrInsertDefault key value map = let (ma, map') = Map.insertLookupWithKey f key value map in (fromMaybe value ma, map')
+    where f _ _ old = old
+
+ingId :: String -> State (Map String Int) Int
+ingId s = State.state f
+    where
+        f map = getOrInsertDefault s (Map.size map) map
+
+
+-- parsePizza :: (Map String Int, [[Int]]) -> [String] -> (Map String Int, [Set Int])
+parsePizza :: [String] -> State (Map String Int) (Set Int)
+parsePizza input = Set.fromList <$> mapM ingId input
+
+parsePizzas :: [[String]] -> [Set Int]
+parsePizzas inp = fst $ State.runState (mapM parsePizza inp) Map.empty
 
 -- Parse file
 parse :: String -> IO (Teams, [Pizza])
 parse file = do
     (teams_line:pizzas) <- lines <$> readFile file
     let teams = triplet $ map read $ tail $ words teams_line
-    let parsePizza = Set.fromList . tail . words
-    let pizzaaas = zip (map parsePizza pizzas) [0..]
+    let pizzaSets = parsePizzas $ map (tail . words) pizzas
+    let pizzaaas = zip pizzaSets [0..]
     return (teams, pizzaaas)
 
 output :: [Output] -> IO ()
@@ -63,7 +89,7 @@ heuristic ((ones, 0, 0), ps) | ones > 0 && length ps >= 2 = let (rest, ids, _) =
     (2, ids):heuristic ((ones-1, 0, 0), rest)
 heuristic _ = []
 
-optim :: Int -> [Pizza] -> ([Pizza], [Int], Set String)
+optim :: Int -> [Pizza] -> ([Pizza], [Int], Set Int)
 optim 0 ps = (ps, [], Set.empty)
 optim x ps = (filter (/= (pizza_ing, pizza_id)) ops, pizza_id:res, Set.union ing pizza_ing)
     where
